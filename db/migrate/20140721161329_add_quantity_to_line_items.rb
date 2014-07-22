4810b1fa02c4bf980436cacd68fdda0f4967170a
tusimbe <tusimbe@gmail.com>
2014-07-22 12:13:28 +0800

    cart done!

---
+++ db/migrate/20140721161329_add_quantity_to_line_items.rb
@@ -0,0 +1,13 @@
+class AddQuantityToLineItems < ActiveRecord::Migration
+  #def change
+  #    add_column :line_items, :quantity, :integer
+  #end
+
+  def self.up
+      add_column :line_items, :quantity, :integer, :default => 1
+  end
+
+  def self.down
+	  remove_column :line_items, :quantity
+  end
+end
